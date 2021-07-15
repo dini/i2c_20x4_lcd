75314eb9e819db03cbea74c0f2272460fa65b8ae
Denis Klester <dinisoft@gmail.com>
2021-07-16 00:28:47 +0600

    fix lint

--- lcd.py
+++ lcd.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# pylint: disable=C0103, C0114
 
-import lcddriver
 from time import sleep
 from datetime import datetime
 import systemd.daemon
 import psutil as ps
 from requests import get
+import lcddriver
 
 I2C_BUS = 1
 
@@ -21,17 +22,20 @@ display = [
 
 
 def get_hwmon():
+    """Get temp from hwmon"""
     with open('/sys/class/hwmon/hwmon0/temp1_input', 'r') as f:
         data = f.read()
         return int(data)
 
 
 def get_ext_ip():
+    """Get external IP"""
     ip = get("https://api.ipify.org").text
     return ip
 
 
 def loadind():
+    """Welcome display"""
     lcd.display_string("####################", 1)
     lcd.display_string("#SYSTEM  MONITORING#", 2)
     lcd.display_string("# LCD SERVICE LOAD #", 3)
@@ -42,6 +46,7 @@ def loadind():
 
 
 def refresh():
+    """Refresh function"""
     cpu_percent = str(ps.cpu_percent()).rjust(4)
     cpu_temp = str(round(get_hwmon()/1000))
     date = datetime.now().strftime('%d.%m')
