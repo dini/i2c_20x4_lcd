55c5df9110050faf041735b77aaba114afe62c71
Denis Klester <dinisoft@gmail.com>
2021-07-18 21:53:23 +0600

    add hddtemp & fix ext ip

--- lcd.py
+++ lcd.py
@@ -6,6 +6,7 @@ from time import sleep
 from datetime import datetime
 import systemd.daemon
 import psutil as ps
+from socket import socket
 from requests import get
 import lcddriver
 
@@ -30,8 +31,43 @@ def get_hwmon():
 
 def get_ext_ip():
     """Get external IP"""
-    ip = get("https://api.ipify.org").text
+    try:
+        ip = get("https://api.ipify.org").text
+    except:
+        ip = "No connection"
     return ip
+
+
+def get_hddtemp():
+    """Get temp from hddtemp"""
+    conn = socket()
+    conn.connect(('127.0.0.1', 7634))
+    data = ''
+    while True:
+        buff = conn.recv(4096)
+        if not buff:
+            break
+        data += buff.decode()
+    return data
+
+
+def hddtemp():
+    """Parse hddtemp"""
+    data = []
+    drive_array = get_hddtemp().split("||")
+    for drive in drive_array:
+        if drive[0] != "|":
+            drive = "|" + drive
+        drive_data = drive.split("|")
+        if drive_data[3] == "ERR":
+            data.append("ERR")
+        elif drive_data[3] == "SLP":
+            data.append("SLP")
+        elif drive_data[4] != "C":
+            data.append("!!!")
+        else:
+            data.append(drive_data[3] + drive_data[4])
+    return data
 
 
 def loadind():
@@ -51,12 +87,19 @@ def refresh():
     cpu_temp = str(round(get_hwmon()/1000))
     date = datetime.now().strftime('%d.%m')
     mem_percent = str(ps.virtual_memory()[2]).rjust(4)
-    time = datetime.now().strftime('%H:%M:%S')
+    time = datetime.now().strftime('%H:%M')
+    hdd = hddtemp()
     lcd.display_string(
-        "CPU:" + cpu_percent + "%" + "  " + cpu_temp + "C " + date, 1
+        "CPU:" + cpu_percent + "%  " + cpu_temp + "C " + date, 1
         )
     lcd.display_string(
-        "MEM:" + mem_percent + "%" + "   " + time, 2
+        "MEM:" + mem_percent + "%      " + time, 2
+        )
+    lcd.display_string(
+        "SDA:" + hdd[0] + "      SDB:" + hdd[1], 3
+        )
+    lcd.display_string(
+        "IP:" + get_ext_ip(), 4
         )
 
 
